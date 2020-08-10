bba436d9c7bfed621d842a801bca7c8e2ecd2254
Mariam Ammar <ammarproductions@gmail.com>
2020-08-10 15:08:06 +0800

    all about data engineering and such

--- my_new_package/lib.py
+++ my_new_package/lib.py
@@ -48,6 +48,10 @@ def clean_data(data):
     return data
 
 
+def try_me(n):
+    return n * 6 +3
+
+
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
     import my_new_package
@@ -55,3 +59,13 @@ if __name__ == '__main__':
     df = pd.read_csv('{}/data/data.csv.gz'.format(folder_source))
     clean_data = clean_data(df)
     print(' dataframe cleaned')
+
+
+
+
+
+
+
+
+
+
